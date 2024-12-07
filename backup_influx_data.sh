a548c4e9b17559fbcbaf75677c2f87ca5979c132
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2024-12-07 12:43:03 +0000

    delete local influx_backup when successfully rsynced

--- backup_influx_data.sh
+++ backup_influx_data.sh
@@ -1,4 +1,4 @@
 influxd backup -portable ./influx_backup/metadata
 influxd backup -portable -database weather ./influx_backup/data/weather
-rsync -avz influx_backup abstractdog@abstractdog.com:.
+rsync -avz influx_backup abstractdog@abstractdog.com:. && rm -rf ./influx_backup/*
 
